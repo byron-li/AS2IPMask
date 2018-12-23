dd6467fac8e127b4a575af5914fbd9887c10cfb1
Li Baiyang <baiyangli@outlook.com>
2018-12-23 12:03:22 +0800

    add asn info in xlsx file

--- AS2IPMask.py
+++ AS2IPMask.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
-# coding: utf-8
+# coding=utf-8
 # Author: BaiyangLi
 
 import os
 import re
 import sys
+import time
 import gzip
 import glob
 import socket
@@ -16,38 +17,26 @@ from openpyxl import Workbook
 from configparser import ConfigParser
 
 
-creation_log = "pfx2as-creation.log"
+download_file = ""
+pfx2as_log = "pfx2as-creation.log"
 
-prefix_v4_url = "http://data.caida.org/datasets/routing/routeviews-prefix2as/"
-prefix_v6_url = "http://data.caida.org/datasets/routing/routeviews6-prefix2as/"
-
-log_v4_url = prefix_v4_url + creation_log
-log_v6_url = prefix_v6_url + creation_log
-
-asn_dict = {}
-conf_dict = {}
-
-conf_name = "as.conf"
-infile_name = ""
-outfile_name = "AS_IP_mapping.xlsx"
-
-del_v4_file = "routeviews-rv2-*-*.pfx2as*"
-del_v6_file = "routeviews-rv6-*-*.pfx2as*"
+asn_ipmask = {}
+asn_info = {}
 
 log_name = "as2ipmask.log"
 log_format = "%(asctime)s [%(levelname)s] %(message)s"
 logging.basicConfig(level=logging.DEBUG, filemode='a', format=log_format, filename=log_name)
 
-socket.setdefaulttimeout(90)
+socket.setdefaulttimeout(30)
 
 
 def _callback_func(num, block_size, total_size):
 	percent = float(num * block_size)/float(total_size) * 100.0
 
 	if((percent - 100) >= 0):
-		sys.stdout.write("\r>> downloading %s %.1f%%\n" % (infile_name, 100))
+		sys.stdout.write("\r>> downloading %s %.1f%%\n" % (download_file, 100))
 	else:
-		sys.stdout.write("\r>> downloading %s %.1f%%" % (infile_name, percent))
+		sys.stdout.write("\r>> downloading %s %.1f%%" % (download_file, percent))
 		sys.stdout.flush()
 
 
@@ -55,83 +44,37 @@ def tail(filename, n):
 	return deque(open(filename), n)
 
 
-def read_conf_file():
-	global conf_dict
-	print(">> reading config file")
-
-	try:
-		with open("./conf/as.conf", "r") as conf_file:
-			conf_header = conf_file.readline().strip('\n')
-			conf_header = "".join(conf_header.split(','))
-			conf_header = "".join(conf_header.split())
-
-			if(conf_header == "RuleNameASN"):
-				conf_line = conf_file.readline().strip('\n')
-
-				while(conf_line):
-					conf_line = "".join(conf_line.split())
-					line_list = conf_line.split(',')
-					
-					rule_name = line_list[0]
-					asn_list = line_list[1:]
-					for asn in asn_list:
-						try:
-							int(asn)
-						except ValueError as e:
-							print(str(e))
-							logging.error("ASN format error: " + str(e))
-							sys.exit()
-						else:
-							if rule_name in conf_dict.keys():
-								if asn in conf_dict[rule_name]:
-									pass
-								else:
-									conf_dict[rule_name].append(asn)
-							else:
-								conf_dict[rule_name] = []
-								conf_dict[rule_name].append(asn)
-					
-					conf_line = conf_file.readline().strip('\n')
-
-			else:
-				print("as.conf file header format error, please check and reopen.")
-				logging.error("as.conf file header format error, check as.conf.")
-				sys.exit()
-
-	except IOError as e:
-		print(e)
-		logging.error(str(e))
-		sys.exit()
-
-
-def download_file(log_url, prefix_url, del_filename):
-	global infile_name
+def download_pfx2as_file(log_url, prefix_url, del_filename):
 	print(">> check pfx2as_file version")
 
+	global download_file
+	latest_file = ""
+
 	try:
-		urllib.request.urlretrieve(log_url, creation_log)
+		urllib.request.urlretrieve(log_url, pfx2as_log)
 
 	except Exception as e:
 		print(str(e))
 		logging.error(str(e))
+		logging.error("Can not check pfx2as file version, program exit.")
 		sys.exit()
 
-	d = tail(creation_log, 1)
-	suffix_url = d[0].split()[2]
+	latest = tail(pfx2as_log, 1)
+	suffix_url = latest[0].split()[2]
 
-	infile_name = suffix_url.split('/')[2]
-	unzipped_name = infile_name.replace(".gz", "")
+	download_file = suffix_url.split('/')[2]
+	latest_file = download_file.replace(".gz", "")
 
-	if os.path.isfile(unzipped_name):
-		infile_name = unzipped_name
+	if os.path.isfile(latest_file):
+		pass
 	else:
-		if glob.glob("./" + del_filename):
-			for path in glob.glob("./" + del_filename):
+		if glob.glob(del_filename):
+			for path in glob.glob(del_filename):
 				os.remove(path)
-			print(">> old routeviews as_file has been removed")
+			print(">> old routeviews files have been removed")
 
 		try:
-			urllib.request.urlretrieve(prefix_url + suffix_url, infile_name, _callback_func)
+			urllib.request.urlretrieve(prefix_url + suffix_url, download_file, _callback_func)
 
 		except Exception as e:
 			print()
@@ -140,16 +83,17 @@ def download_file(log_url, prefix_url, del_filename):
 			sys.exit()
 
 		else:
-			unzip = gzip.GzipFile(mode="rb", fileobj=open(infile_name, 'rb'))
-			infile_name = unzipped_name
-			open(infile_name, "wb").write(unzip.read())
+			unzip = gzip.GzipFile(mode="rb", fileobj=open(download_file, 'rb'))
+			open(latest_file, "wb").write(unzip.read())
+
+	read_pfx2as_file(latest_file)
 
 
-def read_pfx2as_file():
-	global asn_dict
-	print(">> processing file: " + infile_name)
+def read_pfx2as_file(filename):
+	global asn_ipmask
+	print(">> processing file: " + filename)
 
-	with open(infile_name, "r") as input_file:
+	with open(filename, "r") as input_file:
 		infile_line = input_file.readline().strip('\n')
 
 		while(infile_line):
@@ -174,38 +118,113 @@ def read_pfx2as_file():
 
 			for as_num in as_list:
 				for sub_ip_mask in sub_ip_list:
-					if as_num in asn_dict.keys():
-						if str(sub_ip_mask) in asn_dict[as_num]:
+					if as_num in asn_ipmask.keys():
+						if str(sub_ip_mask) in asn_ipmask[as_num]:
 							pass
 						else:
-							asn_dict[as_num].append(str(sub_ip_mask))
+							asn_ipmask[as_num].append(str(sub_ip_mask))
 					else:
-						asn_dict[as_num] = []
-						asn_dict[as_num].append(str(sub_ip_mask))
+						asn_ipmask[as_num] = []
+						asn_ipmask[as_num].append(str(sub_ip_mask))
 			
 			infile_line = input_file.readline().strip('\n')
 
 
-def write_to_excel():
+def download_asn_info():
+	global asn_info
+	global download_file
+
+	download_file = "asn_info"
+	del_asn_info = "asn_info_*"
+	asn_info_url = "https://www.cidr-report.org/as2.0/autnums.html"
+	asn_info_file = download_file + "_" + time.strftime("%Y%m%d", time.localtime())
+
+	if os.path.isfile(asn_info_file):
+		pass
+	
+	else:
+		try:
+			urllib.request.urlretrieve(asn_info_url, download_file, _callback_func)
+
+		except Exception as e:
+			print()
+			print(str(e))
+			logging.error(str(e))
+
+			if(glob.glob(del_asn_info)):
+				logging.error("Failed to download the latest ASN_INFO File, use the previous version.")
+				asn_info_file = glob.glob(del_asn_info)[0]
+
+			else:
+				logging.error("Failed to download the latest ASN_INFO File and no previous version exists. No ASN_INFO records in xlsx file.")
+				asn_info_file = ""
+
+		else:
+			if glob.glob(del_asn_info):
+				for path in glob.glob(del_asn_info):
+					os.remove(path)
+			print(">> old asn_info file has been removed")
+			os.rename(download_file, asn_info_file)
+
+	if(asn_info_file):
+		with open(asn_info_file, "r", encoding = "ISO-8859-1") as input_file:
+			infile_line = input_file.readline().strip('\n')
+			pattern = re.compile(r'<a href="/cgi-bin/as-report\?as=.*">AS(.*)</a>(.*)')
+
+			while(infile_line):
+				result = pattern.findall(infile_line)
+				if(result):
+					key = result[0][0].strip()
+					value = result[0][1].strip()
+					asn_info[key] = value
+
+				infile_line = input_file.readline().strip('\n')
+
+
+def write_to_excel(conf_list):
 	print(">> saving configuration to excel")
-	as_count = 1
-	title = ['Rule Name','ASN','Server IP']
+
+	outfile_name = "AS_IP_mapping.xlsx"
+	title = ['AS Name', 'ASN', 'Server IP', 'Details', 'Country Code']
 	wb = Workbook()
 	ws = wb.active
 	ws.append(title)
 
-	for rule_name in conf_dict:
-		for asn in conf_dict[rule_name]:
-			if asn in asn_dict.keys():
-				for ip_mask in asn_dict[asn]:
+	for asn in conf_list:
+		asn = asn.strip()
+
+		if(asn):
+			if asn in asn_ipmask.keys():
+				as_name = ""
+				details = ""
+				country_code = ""
+
+				if asn in asn_info.keys():
+					details = asn_info[asn]
+					asn_info_list1 = details.split(" - ")
+					asn_info_list2 = details.split(", ")
+
+					if(len(asn_info_list1) > 1):
+						as_name = asn_info_list1[0]
+					else:
+						as_name = asn_info_list2[0]
+
+					country_code = asn_info_list2[-1]
+
+				for ip_mask in asn_ipmask[asn]:
 					row = []
-					row.append(rule_name)
+					row.append(as_name)
 					row.append(asn)
 					row.append(ip_mask)
+					row.append(details)
+					row.append(country_code)
 					ws.append(row)
+					
 			else:
-				print("Warning: AS_num is not in CAIDA file, Your configuration may not take effect!")
-				logging.warning("ASN " + str(asn) + " is not in CAIDA file, Your configuration may not take effect!")
+				print("Warning: AS" + str(asn) + " is not in CAIDA file, Your configuration may not take effect!")
+				logging.warning("AS" + str(asn) + " is not in CAIDA file, Your configuration may not take effect!")
+		else:
+			continue
 
 	try:
 		wb.save(outfile_name)
@@ -217,39 +236,47 @@ def write_to_excel():
 
 
 def main():
-	cfg = ConfigParser()
+	print(">> reading config file")
 
+	prefix_v4_url = "http://data.caida.org/datasets/routing/routeviews-prefix2as/"
+	prefix_v6_url = "http://data.caida.org/datasets/routing/routeviews6-prefix2as/"
+
+	log_v4_url = prefix_v4_url + pfx2as_log
+	log_v6_url = prefix_v6_url + pfx2as_log
+
+	del_v4_file = "routeviews-rv2-*-*.pfx2as*"
+	del_v6_file = "routeviews-rv6-*-*.pfx2as*"
+
+	cfg = ConfigParser()
+	asn_conf = []
+	
 	try:
 		cfg.read("./conf/config.ini")
-		option = cfg.get("ip", "ip_version")
+		ip_ver = cfg.get("IP", "IPVERSION")
+		asn_conf = cfg.get("ASN", "ASNUMBER").split(",")
 
 	except Exception as e:
 		print(str(e) + ", check config.ini.")
 		logging.error(str(e) + ", check config.ini.")
 		sys.exit()
 
-	read_conf_file()
+	if(ip_ver == "4"):
+		download_pfx2as_file(log_v4_url, prefix_v4_url, del_v4_file)
 
-	if(option == "4"):
-		download_file(log_v4_url, prefix_v4_url, del_v4_file)
-		read_pfx2as_file()
+	elif(ip_ver == "6"):
+		download_pfx2as_file(log_v6_url, prefix_v6_url, del_v6_file)
 
-	elif(option == "6"):
-		download_file(log_v6_url, prefix_v6_url, del_v6_file)
-		read_pfx2as_file()
-
-	elif(option.lower() == "all"):
-		download_file(log_v4_url, prefix_v4_url, del_v4_file)
-		read_pfx2as_file()
-		download_file(log_v6_url, prefix_v6_url, del_v6_file)
-		read_pfx2as_file()
+	elif(ip_ver.lower() == "all"):
+		download_pfx2as_file(log_v4_url, prefix_v4_url, del_v4_file)
+		download_pfx2as_file(log_v6_url, prefix_v6_url, del_v6_file)
 
 	else:
 		print("IP version value error, check config.ini.")
 		logging.error("IP version value error, check config.ini.")
 		sys.exit()
 
-	write_to_excel()
+	download_asn_info()
+	write_to_excel(asn_conf)
 	os.system("pause")
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -1,34 +1,32 @@
-# AS2IPMask:
+## AS2IPMask:
 
-从CAIDA获取最新pfx2as数据, 根据配置ASN, 输出IP地址段(掩码>=16)的xlsx格式文件.
+*	从 [CAIDA](https://www.caida.org/data/routing/routeviews-prefix2as.xml) 获取最新 pfx2as 数据
+*	从 [CIDR](http://www.cidr-report.org/as2.0/) 获取ASN注册信息
+*	根据配置 ASN, 输出其 IP 地址段 (掩码格式转换为>=16) 及相关注册信息, 保存为 xlsx 格式文件
 
-## 配置
+### 配置
 
-*	配置文件位于conf目录下
-	*	config.ini: 程序配置文件, 指定获取对应ASN对应IP段: IPv4(4) /IPv6(6)/ 全部(all).
-	*	as.conf: ASN配置文件, 输入RuleName及ASN, 逗号分隔. 一条规则下有多个ASN也需用逗号分隔.  
-		e.g. test,4808,6836
+*	配置文件位于 conf 目录下
+	*	config.ini: 程序配置文件, 包含 [IP], [ASN] 两 section
+		*	IP Sect: 指定获取对应ASN对应IP段: IPv4(4) /IPv6(6)/ 全部(all)
+		*	ASN Sect: 输入 ASN, 逗号分隔
 
-## 输出
+###	逻辑
+*	从配置文件中读取 ASN, 检查 ASN 是否在 pfx2as 中
+	*	若文件中存在该 ASN, 填充 ASN info, 写入 xlsx
+	*	若不存在, 记入日志, 不写入结果文件
 
-*	每次输出覆盖上一次生成结果, 文件名: AS_IP_mapping.xlsx
+
+### 输出
+
+*	全量式输出, 覆盖上一次结果, 输出文件名: AS_IP_mapping.xlsx
 *	数据示例  
 
+|	AS Name		|	ASN		|	Server IP	|	Details	|Country Code |  
+ :---: | :---:| :---:|:---: | :---:
+|7171-W-95TH-STREET|23333|	209.201.98.0/24|7171-W-95TH-STREET - WeightWatchers.com, Inc., US|US|  
 
-	| Rule Name | ASN   | Server IP        |
-	| --------- | ----- | ---------------- |
-	| test      | 60140 | 185.56.88.0/22   |
-	| test      | 60140 | 185.140.248.0/24 |
-	| test      | 60140 | 185.140.249.0/24 |
-	| test      | 60140 | 185.140.250.0/24 |
-	| test      | 60140 | 185.140.251.0/24 |
-	| test      | 60140 | 185.153.148.0/24 |
-	| test      | 60140 | 185.160.64.0/24  |
-	| test      | 60140 | 185.161.68.0/24  |
-	| test      | 60140 | 185.201.189.0/24 |
-	| test      | 60140 | 185.201.191.0/24 |
-	| test      | 60140 | 194.150.72.0/24  |
 
-## 日志
+### 日志
 
-*	程序异常退出日志记录在as2ipmask.log文件中.
+*	程序运行日志记录在 as2ipmask.log 文件中
