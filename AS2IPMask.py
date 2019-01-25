60f74dab2183565e9b5e3caf8291b5399fe5df01
byron-li <baiyangli@outlook.com>
2019-01-25 16:32:17 +0800

    fix bugs caused by file path

--- AS2IPMask.py
+++ AS2IPMask.py
@@ -19,13 +19,18 @@ from configparser import ConfigParser
 
 asn_info = {}
 
-download_file = ""
-pfx2as_log = "pfx2as-creation.log"
-
 socket.setdefaulttimeout(30)
 ssl._create_default_https_context = ssl._create_unverified_context
 
-log_name = "as2ipmask.log"
+localtime = time.strftime("%Y%m%d", time.localtime())
+
+dir_path = os.path.realpath(sys.argv[0])
+dir_path = dir_path.replace(dir_path.split(os.sep)[-1], "")
+
+download_file = ""
+pfx2as_log = "pfx2as-creation.log"
+
+log_name = dir_path + "as2ipmask.log"
 log_format = "%(asctime)s [%(levelname)s] %(message)s"
 logging.basicConfig(level=logging.DEBUG, filemode='a', format=log_format, filename=log_name)
 
@@ -91,7 +96,7 @@ def download_pfx2as_file(log_url, prefix_url, del_filename):
 	latest_file = ""
 
 	try:
-		urllib.request.urlretrieve(log_url, pfx2as_log)
+		urllib.request.urlretrieve(log_url, dir_path+pfx2as_log)
 
 	except Exception as e:
 		print(str(e))
@@ -99,11 +104,11 @@ def download_pfx2as_file(log_url, prefix_url, del_filename):
 		logging.error("Can not check pfx2as file version, program exit.")
 		sys.exit()
 
-	latest = tail(pfx2as_log, 1)
+	latest = tail(dir_path+pfx2as_log, 1)
 
 	try:
 		suffix_url = latest[0].split()[2]
-		download_file = suffix_url.split('/')[2]
+		download_file = dir_path + suffix_url.split('/')[2]
 		latest_file = download_file.replace(".gz", "")
 
 	except Exception as e:
@@ -121,7 +126,7 @@ def download_pfx2as_file(log_url, prefix_url, del_filename):
 			print(">> old routeviews files have been removed")
 
 		try:
-			urllib.request.urlretrieve(prefix_url + suffix_url, download_file, _callback_func)
+			urllib.request.urlretrieve(prefix_url+suffix_url, download_file, _callback_func)
 
 		except Exception as e:
 			print()
@@ -160,9 +165,9 @@ def download_asn_info():
 	global download_file
 
 	asn_info_file = ""
-	download_file = "asn_info"
-	del_asn_info = "asn_info_*"
-	asn_info_latest = download_file + "_" + time.strftime("%Y%m%d", time.localtime())
+	download_file = dir_path + "asn_info"
+	del_asn_info = dir_path + "asn_info_*"
+	asn_info_latest = download_file + "_" + localtime
 
 	asn_info_url = "https://www.cidr-report.org/as2.0/autnums.html"
 
@@ -238,7 +243,12 @@ def lookup_asn_info(asn):
 	return asn_item_info
 
 
-def write_to_excel(conf_list, asn_ipmask, outfile_name):
+def write_to_excel(conf_list, asn_ipmask, outfile_name, del_filename):
+	if glob.glob(del_filename):
+		for path in glob.glob(del_filename):
+			os.remove(path)
+		print(">> old as_ip_mapping xlsx file has been removed")
+
 	print(">> saving configuration to excel")
 
 	title = ['AS Name', 'ASN', 'Server IP', 'Details', 'Country Code']
@@ -300,13 +310,16 @@ def main():
 	log_v4_url = prefix_v4_url + pfx2as_log
 	log_v6_url = prefix_v6_url + pfx2as_log
 
-	del_v4_file = "routeviews-rv2-*-*.pfx2as*"
-	del_v6_file = "routeviews-rv6-*-*.pfx2as*"
+	del_v4_file = dir_path + "routeviews-rv2-*-*.pfx2as*"
+	del_v6_file = dir_path + "routeviews-rv6-*-*.pfx2as*"
+
+	del_v4_xlsx = dir_path + "AS_IP_mapping_v4_*.xlsx"
+	del_v6_xlsx = dir_path + "AS_IP_mapping_v6_*.xlsx"
 
 	cfg = ConfigParser()
 	
-	out_file_v4 = "AS_IP_mapping_v4_" + time.strftime("%Y%m%d", time.localtime()) + ".xlsx"
-	out_file_v6 = "AS_IP_mapping_v6_" + time.strftime("%Y%m%d", time.localtime()) + ".xlsx"
+	out_file_v4 = dir_path + "AS_IP_mapping_v4_" + localtime + ".xlsx"
+	out_file_v6 = dir_path + "AS_IP_mapping_v6_" + localtime + ".xlsx"
 	pfx2as_file_v4 = ""
 	pfx2as_file_v6 = ""
 	
@@ -315,7 +328,7 @@ def main():
 	asn_conf = []
 	
 	try:
-		cfg.read("./conf/config.ini")
+		cfg.read(dir_path + "conf" + os.sep + "config.ini")
 		ip_ver = cfg.get("IP", "IPVERSION")
 		asn_conf = cfg.get("ASN", "ASNUMBER").split(",")
 
@@ -327,31 +340,29 @@ def main():
 	download_asn_info()
 
 	if(ip_ver == "4"):
-		out_file_v4 = "AS_IP_mapping_v4.xlsx"
 		pfx2as_file_v4 = download_pfx2as_file(log_v4_url, prefix_v4_url, del_v4_file)
 		read_pfx2as_file(pfx2as_file_v4, asn_ipmask_v4)
-		write_to_excel(asn_conf, asn_ipmask_v4, out_file_v4)
+		write_to_excel(asn_conf, asn_ipmask_v4, out_file_v4, del_v4_xlsx)
 
 	elif(ip_ver == "6"):
-		out_file = "AS_IP_mapping_v6.xlsx"
 		pfx2as_file_v6 = download_pfx2as_file(log_v6_url, prefix_v6_url, del_v6_file)
 		read_pfx2as_file(pfx2as_file_v6, asn_ipmask_v6)
-		write_to_excel(asn_conf, asn_ipmask_v6, out_file_v6)
+		write_to_excel(asn_conf, asn_ipmask_v6, out_file_v6, del_v6_xlsx)
 
 	elif(ip_ver.lower() == "all"):
 		pfx2as_file_v4 = download_pfx2as_file(log_v4_url, prefix_v4_url, del_v4_file)
 		pfx2as_file_v6 = download_pfx2as_file(log_v6_url, prefix_v6_url, del_v6_file)
 		read_pfx2as_file(pfx2as_file_v4, asn_ipmask_v4)
 		read_pfx2as_file(pfx2as_file_v6, asn_ipmask_v6)
-		write_to_excel(asn_conf, asn_ipmask_v4, out_file_v4)
-		write_to_excel(asn_conf, asn_ipmask_v6, out_file_v6)
+		write_to_excel(asn_conf, asn_ipmask_v4, out_file_v4, del_v4_xlsx)
+		write_to_excel(asn_conf, asn_ipmask_v6, out_file_v6, del_v6_xlsx)
 
 	else:
 		print("IP version value error, check config.ini.")
 		logging.error("IP version value error, check config.ini.")
 		sys.exit()
 
-	# os.system("pause")
+	os.system("pause")
 
 
 if __name__ == '__main__':
